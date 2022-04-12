7742ccb7ab559f4e2125b58ffbc5a4c289a761b6
savy7y <93451471+savy7y@users.noreply.github.com>
2022-04-12 22:06:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col h-screen justify-between">
       <Navbar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Footer />
