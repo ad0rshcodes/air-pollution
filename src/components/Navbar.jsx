95eeee279a59667e9148b2463b2e9cf61761b9ce
adarsh-sharma6218 <adarshsharma6218@gmail.com>
2022-04-12 23:10:04 +0530

    feat: Navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 // Created by Adarsh Sharma
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div class="Navbar flex justify-between mx-4 my-3 bg-transparent bg-opacity-50">
+        <div className="logo flex">Logo</div>
+        <div className="links flex">
+          <a href="./" class="mx-2">
+            Home
+          </a>
+          <a href="./" class="mx-2">
+            Prevention
+          </a>
+          <a href="./" class="mx-2">
+            Sources
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
