bcc64375f836af54c51a02fbac1b008ec59c5fce
savy7y <93451471+savy7y@users.noreply.github.com>
2022-04-12 22:50:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React, { Component } from 'react';
+import "./index.css"
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
-    <div className="App flex flex-col h-screen justify-between">
+    <div className="main App flex flex-col h-screen justify-between">
       <Navbar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Footer />
