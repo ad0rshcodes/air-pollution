397f6f01c5abb3885e653a73d96772300872afad
savy7y <93451471+savy7y@users.noreply.github.com>
2022-04-12 19:28:33 +0530

    Update: tailwind.config,js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'darkblue' : '#1a374d',
+      'light' : '#b1d0e0',
+      'blue' : '#6998ab',
+    },
     extend: {},
   },
   plugins: [],
