ea454be5d0da87909b692ef89a21442d9d8e900e
adarsharma-in <hello@adarsharma.in>
2022-04-13 13:33:31 +0530

    chore: Check commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <div class="flex justify-between my-3">
+      <div class="flex justify-between my-3 text-lg	">
         <div className="logo flex">Logo</div>
         <div className="links flex space-x-4">
           <a href="./" class="">
