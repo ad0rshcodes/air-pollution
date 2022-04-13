cce9d63fd26cc18c8c2917eaea60abdafa9cb23a
savy7y <93451471+savy7y@users.noreply.github.com>
2022-04-14 00:25:21 +0530

    fix: responsiveness

--- src/components/Co2Graph.jsx
+++ src/components/Co2Graph.jsx
@@ -23,20 +23,20 @@ export default function Co2Graph() {
   });
   return (
     <>
-      <div className="CO2 flex justify-between items-center mb-10 flex-wrap">
-        <div style={{ width: "50%" }}>
+      <div className="l-cont-c ">
+        <div className="l-cont-g">
           <LineChart chartData={userData} />
         </div>
-        <div className="CO2-description w-1/2">
-          Air pollution is an issue that has been plaguing the world for a long
+        <div className='l-cont-p'>
+        Air pollution is an issue that has been plaguing the world for a long
           time now and its detrimental effects urge us to find ways to predict
           and prevent further damage. According to WHO, ambient air pollution
           accounts for an estimated 4.2 million deaths per year due to stroke,
           heart disease, lung cancer, and chronic respiratory diseases. Such
           statistical data can prove to be of great significance in the process
-          of polic
         </div>
       </div>
     </>
   );
 }
+
