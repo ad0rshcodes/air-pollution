bdd396406a0e82099acb9a8b7eef767a812a1db4
savy7y <93451471+savy7y@users.noreply.github.com>
2022-04-14 00:18:03 +0530

    feat; Add Sources page

---
+++ src/components/Sources.jsx
@@ -0,0 +1,16 @@
+export default function Sources(){
+    return(<>
+    
+    <div className="src-h1">
+        Data Sources:
+    </div>
+    <ui className="src-ui">
+        <li><a href="www.wikipedia.org">wikipedia.org</a></li>
+        <li><a href="www.climate.nasa.gov">climate.nasa.gov</a></li>       
+        <li><a href="www.lung.org/lung-health-diseases">lung.org</a></li>
+            
+    </ui>
+
+    </>
+    );
+}
