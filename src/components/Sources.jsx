ad170e5057af58c640140ae87067e797d899193e
savy7y <93451471+savy7y@users.noreply.github.com>
2022-04-14 13:43:43 +0530

    fix: data sources

--- src/components/Sources.jsx
+++ src/components/Sources.jsx
@@ -6,11 +6,14 @@ export default function Sources(){
         Data Sources:
     </div>
     <ui className="src-ui">
-        <li><a href="https://www.wikipedia.org">wikipedia.org</a></li>
-        <li><a href="https://www.climate.nasa.gov">climate.nasa.gov</a></li>       
-        <li><a href="https://www.lung.org/lung-health-diseases">lung.org</a></li>
-        <li><a href="https://www.pexels.com" >pexels.com</a></li>
-        <li><a href="https://www.ncbi.nlm.nih.gov">www.ncbi.nlm.nih.gov</a></li>
+        <li><a href="https://www.wikipedia.org" target="_blank">wikipedia.org</a></li>
+        <li><a href="https://climate.nasa.gov/vital-signs/carbon-dioxide/" target="_blank">climate.nasa.gov</a></li>       
+        <li><a href="https://www.lung.org/lung-health-diseases" target="_blank">lung.org</a></li>
+        <li><a href="https://www.pexels.com" target="_blank" >pexels.com</a></li>
+        <li><a href="https://www.ncbi.nlm.nih.gov" target="_blank">www.ncbi.nlm.nih.gov</a></li>
+        <li><a href="https://www.who.int/health-topics/air-pollution" target="_blank">who.int</a></li>
+        <li><a href="http://www.scienceing.com/" target="_blank"></a>scienceing.com</li>
+        <li><a href="https://www.sciencedirect.com/science/article/pii/B9780128167786000011" target="_blank">sciencedirect.com</a></li>
     </ui>
 
     </>
